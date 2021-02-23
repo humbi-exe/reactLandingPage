38043a025a199c4929170eb27cc802ed495a8ad9
humbi-exe <humberto.becerra95@gmail.com>
2021-02-23 17:01:41 -0400

    entrega

--- src/js/component/body.js
+++ src/js/component/body.js
@@ -4,20 +4,120 @@ const Body = () => {
 	return (
 		<React.Fragment>
 			<div className="container">
-				<div className="col-md-3">
-					<div className="row">
-						<div className="card" style="width: 18rem;">
-							<img
-								className="card-img-top"
-								src="..."
-								alt="Card image cap"
-							/>
-							<div className="card-body">
-								<p className="card-text">
-									{
-										"Some quick example text to build on the card title and make up the bulk of the card's content."
-									}
-								</p>
+				<div className="row">
+					<div className="col-md-3">
+						<div className="card-group">
+							<div className="card mb-5">
+								<img
+									src="http://lorempixel.com/g/400/200"
+									className="card-img-top"
+									alt=""
+								/>
+								<div className="card-body">
+									<h5 className="card-title">
+										{"Card Title"}
+									</h5>
+									<p className="card-text">
+										{
+											"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+										}
+									</p>
+								</div>
+								<div className="card-footer">
+									<a
+										className="btn btn-primary btn-lg d-flex justify-content-center"
+										href="#"
+										role="button">
+										{"Learn more"}
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-3">
+						<div className="card-group">
+							<div className="card">
+								<img
+									src="https://picsum.photos/seed/picsum/400/200"
+									className="card-img-top"
+									alt=""
+								/>
+								<div className="card-body">
+									<h5 className="card-title">
+										{"Card Title"}
+									</h5>
+									<p className="card-text">
+										{
+											"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+										}
+									</p>
+								</div>
+								<div className="card-footer">
+									<a
+										className="btn btn-primary btn-lg d-flex justify-content-center"
+										href="#"
+										role="button">
+										{"Learn more"}
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-3">
+						<div className="card-group">
+							<div className="card">
+								<img
+									src="https://picsum.photos/id/237/400/200"
+									className="card-img-top"
+									alt=""
+								/>
+								<div className="card-body">
+									<h5 className="card-title">
+										{"Card Title"}
+									</h5>
+									<p className="card-text">
+										{
+											"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+										}
+									</p>
+								</div>
+								<div className="card-footer">
+									<a
+										className="btn btn-primary btn-lg d-flex justify-content-center"
+										href="#"
+										role="button">
+										{"Learn more"}
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-3">
+						<div className="card-group">
+							<div className="card">
+								<img
+									src="https://picsum.photos/400/200/?blur"
+									className="card-img-top"
+									alt=""
+								/>
+								<div className="card-body">
+									<h5 className="card-title">
+										{"Card Title"}
+									</h5>
+									<p className="card-text">
+										{
+											"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+										}
+									</p>
+								</div>
+								<div className="card-footer">
+									<a
+										className="btn btn-primary btn-lg d-flex justify-content-center"
+										href="#"
+										role="button">
+										{"Learn more"}
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
